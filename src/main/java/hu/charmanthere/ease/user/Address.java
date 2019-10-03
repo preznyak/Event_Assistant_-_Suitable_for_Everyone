0848eec9d5a102f557b68ba20e2115a50e118c54
Preznyák László <laszlopreznyak11@gmail.com>
2019-10-03 14:19:20 +0200

    User and Address entity created

---
+++ src/main/java/hu/charmanthere/ease/user/Address.java
@@ -0,0 +1,89 @@
+package hu.charmanthere.ease.user;
+
+import javax.persistence.*;
+
+@Entity(name = "address")
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long addressId;
+
+    @OneToOne
+    private User user;
+
+    private String houseNumber;
+    private String streetName;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    public Address() {
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/hu/charmanthere/ease/user/User.java
@@ -0,0 +1,91 @@
+package hu.charmanthere.ease.user;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+
+@Entity(name = "user")
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long userId;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private LocalDate birthDate;
+
+    private LocalDate registrationDate;
+
+    private LocalDate lastLoginDate;
+
+    @OneToOne(orphanRemoval = true, targetEntity = Address.class)
+    private Address address;
+
+    //TODO Preferences, TimeZone, Language, etc
+
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDate getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    public void setLastLoginDate(LocalDate lastLoginDate) {
+        this.lastLoginDate = lastLoginDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
